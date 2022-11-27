db8d167c852d6638b2318b11bdf4712306899956
Asan Azimkulov <azimkulov40@gmail.com>
2022-11-27 14:31:28 +0600

    верстает блок «Предварительный расчёт»

--- bundle/js/main.js
+++ bundle/js/main.js
@@ -32,11 +32,40 @@ $('.stages .slider').on('transition-start', function (e) {
   $('.stages__top li').removeClass('stages__top__item--active');
   $('.stages__top li.stages__top__item').eq($('.stages .slider__indicators li.active').attr('data-slide-to')).addClass('stages__top__item--active');
 });
-$('.vidgets .contact__main *').hover(() => {
+
+
+function isEventInElement(event, element) {
+  var rect = element.getBoundingClientRect();
+  var x = event.clientX;
+  if (x < rect.left || x >= rect.right) return false;
+  var y = event.clientY;
+  if (y < rect.top || y >= rect.bottom) return false;
+  return true;
+}
+
+const vidgets = [document.querySelector('.vidgets .contact__main .vidgets__button')].concat(document.querySelector('.vidgets .calculate .vidgets__button'));
+
+
+document.addEventListener('mousemove', (evt) => {
+  const needOpen = vidgets.some(vidget => isEventInElement(evt, vidget));
+
+  if (needOpen) {
+    document.querySelector('.vidgets').classList.add('vidgets--hover');
+  }
+
+})
+
+$('.vidgets').on('mouseleave', function (e)  {
+  $(this).removeClass('vidgets--hover')
+  console.log(2)
+})
+
+
+$('.vidgets .contact__main .vidgets__button').hover(() => {
   $('.vidgets .contact').addClass('contact--visible');
 })
 
-$('.vidgets .calculate *').hover(() => {
+$('.vidgets .calculate .vidgets__button').hover(() => {
   $('.vidgets .calculate').addClass('calculate--visible');
 })
 
--- js/vidgets.js
+++ js/vidgets.js
@@ -1,8 +1,37 @@
-$('.vidgets .contact__main *').hover(() => {
+
+
+function isEventInElement(event, element) {
+  var rect = element.getBoundingClientRect();
+  var x = event.clientX;
+  if (x < rect.left || x >= rect.right) return false;
+  var y = event.clientY;
+  if (y < rect.top || y >= rect.bottom) return false;
+  return true;
+}
+
+const vidgets = [document.querySelector('.vidgets .contact__main .vidgets__button')].concat(document.querySelector('.vidgets .calculate .vidgets__button'));
+
+
+document.addEventListener('mousemove', (evt) => {
+  const needOpen = vidgets.some(vidget => isEventInElement(evt, vidget));
+
+  if (needOpen) {
+    document.querySelector('.vidgets').classList.add('vidgets--hover');
+  }
+
+})
+
+$('.vidgets').on('mouseleave', function (e)  {
+  $(this).removeClass('vidgets--hover')
+  console.log(2)
+})
+
+
+$('.vidgets .contact__main .vidgets__button').hover(() => {
   $('.vidgets .contact').addClass('contact--visible');
 })
 
-$('.vidgets .calculate *').hover(() => {
+$('.vidgets .calculate .vidgets__button').hover(() => {
   $('.vidgets .calculate').addClass('calculate--visible');
 })
 
